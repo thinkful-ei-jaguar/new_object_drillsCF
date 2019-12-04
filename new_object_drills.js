28db538c8fe333118d0764349a2477182f2cc625
Catrina Fuentes <cat.fuentes93@gmail.com>
2019-12-03 21:35:58 -0800

    added arrays of objects drill (first attempt)

--- new_object_drills.js
+++ new_object_drills.js
@@ -28,7 +28,42 @@ for (const prop in myObj) {
 //Arrays In Objects
 
 const food = {
-    meals = ["breakfast", "second breakfast", "elveness", "lunch", "afternoon tea", "dinner", "supper"]
+    meals: ["breakfast", "second breakfast", "elveness", "lunch", "afternoon tea", "dinner", "supper"],
 };
 
-console.log(food[3]);
+console.log(food.meals[3]);
+
+//Arrays Of Objects
+
+let occupations = {
+    occu1: {
+        name: "Linda Belcher",
+        jobTitle: "Waitress"
+    },
+
+    occu2: {
+        name: "Bob Belcher",
+        jobTitle: "Chef"
+    },
+
+    occu3: {
+        name: "Bob Belcher",
+        jobTitle: "Chef"
+    },
+
+    occu4: {
+        name: "Louise Belcher",
+        jobTitle: "Mischief Maker"
+    },
+
+    occu5: {
+        name: "Tina Belcher",
+        jobTitle: "Zombie Fiction Writer"
+    },
+};
+
+let allJobs = [];
+
+for(let prop in occupations) {
+    allJobs.push(occupations[prop]);
+}
